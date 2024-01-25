62bf560ed10000600ab1f3b42bca87644523f7a1
Soso Khurtsidze <sosokhurtsidzee@gmail.com>
2024-01-25 17:12:27 +0400

    update sign up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, Inject, OnChanges, OnInit, PLATFORM_ID, SimpleChanges} from '@angular/core';
+import {AuthService} from "./auth/auth.service";
+import {BehaviorSubject} from "rxjs";
+import {isPlatformBrowser, isPlatformServer} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'email-client';
+export class AppComponent implements OnInit {
+  signedIn$!: BehaviorSubject<boolean>;
+
+  constructor(private authService: AuthService, @Inject(PLATFORM_ID) private platformId: Object) {
+    this.signedIn$ = this.authService.signedIn$;
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.authService.checkAuth().subscribe();
+    }
+  }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {SignupCredentials, SignupResponse} from "./auth.model";
+import {BehaviorSubject, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   rootUrl = 'https://api.angular-email.com';
-
+  signedIn$ = new BehaviorSubject(false);
 
   constructor(private http: HttpClient) {
   }
@@ -19,7 +20,21 @@ export class AuthService {
   }
 
   signup(credentials: SignupCredentials) {
-    return this.http.post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials)
+    return this.http.post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials, {
+      withCredentials: true
+    })
+      .pipe(
+        tap(() => this.signedIn$.next(true))
+      )
   }
+
+  checkAuth() {
+    return this.http.get(`${this.rootUrl}/auth/signedin`, {
+      withCredentials: true
+    }).pipe(
+      tap()
+    )
+  }
+
 
 }
